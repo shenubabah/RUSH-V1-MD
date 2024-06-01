1b6b2fb4eb9f9438b671fe15d69e07f1d66935d2
cutibabah <dilshanrashmika128@gmail.com>
2024-06-02 00:27:18 +0530

    Update Tempmail.js

--- commandes/Tempmail.js
+++ commandes/Tempmail.js
@@ -3,10 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "tempmail", reaction: "😾", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '*DEXTER-MD* Temporary emails,Powered by *France King* \n\n ' + "Click the link below to create unlimited TEMPORARY Emails. Powered by *DEXTER-MD.*";
+    let z = '*RUSH-MD* Temporary emails,Powered by *rush* \n\n ' + "Click the link below to create unlimited TEMPORARY Emails. Powered by *RUSH-MD.*";
     let d = ' https://email10min.com';
     let varmess = z + d;
-    var img = 'https://telegra.ph/file/7a8bf9f622dd6ccb403df.mp4';
+    var img = 'https://telegra.ph/file/cba60a28d9015165320c9.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
     //console.log("montest")
 });
@@ -22,6 +22,6 @@ async function  execute  (origineMessage,zok) {
    let z ='Salut je m\'appelle *FLASH-MD* \n\n '+'je suis un bot Whatsapp Multi-appareil '
       let d =' developpé par *France King*'
       let varmess=z+d
-      var img='https://telegra.ph/file/13d63c21c1a665bfd8324.jpg'
+      var img='https://telegra.ph/file/cba60a28d9015165320c9.jpg'
 await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
 }  */ 
