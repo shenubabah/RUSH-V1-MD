730b3f7d875f22a43b9dafb403a1d10a62fd1ca5
cutibabah <dilshanrashmika128@gmail.com>
2024-06-02 00:12:09 +0530

    Update fancy.js

--- commandes/fancy.js
+++ commandes/fancy.js
@@ -8,7 +8,7 @@ zokou({ nomCom: "fancy", categorie: "Fun", reaction: "☑️" }, async (dest, zk
 
     try {
         if (id === undefined || text === undefined) {
-            return await repondre(`\nExample : ${prefixe}fancy 10 Dexter-Md\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('DEXTER-MD', fancy));
+            return await repondre(`\nExample : ${prefixe}fancy 10 rush-v1-Md\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('RUSH-MD', fancy));
         }
 
         const selectedStyle = fancy[parseInt(id) - 1];
