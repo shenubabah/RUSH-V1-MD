217da30bd9a6cb62b9723786bfcbe7e665b836b4
cutibabah <dilshanrashmika128@gmail.com>
2024-06-01 23:05:55 +0530

    Update stickersearch.js

--- commandes/stickersearch.js
+++ commandes/stickersearch.js
@@ -5,7 +5,7 @@ const {zokou} = require("../framework/zokou");
 zokou({
   nomCom: "stickersearch",
   categorie: 'Search',
-  reaction: "🍁"
+  reaction: "🇱🇰"
 },
 async (dest, zk, commandeOptions) => {
   const { repondre, ms, arg, nomAuteurMessage } = commandeOptions;
@@ -33,7 +33,7 @@ async (dest, zk, commandeOptions) => {
 
     const stickerMess = new Sticker(gifUrl, {
       pack: packname,
-      author: 'DEXTER-MD',
+      author: 'RUSH-MD',
       type: StickerTypes.FULL,
       categories: ["🤩", "🎉"],
       id: "12345",
